3aeff1dfb5b2bcb4b88a0889b5e9575d7151ecd7
matheusbastosbarbosaUcsal <matheusbastos.barbosa@ucsal.edu.br>
2025-11-19 17:57:53 -0300

    Implementado sistema antifraude usando reflection

--- src/main/java/edu/ucsal/fiadopay/service/PaymentService.java
+++ src/main/java/edu/ucsal/fiadopay/service/PaymentService.java
@@ -6,6 +6,8 @@ import edu.ucsal.fiadopay.dto.PaymentRequest;
 import edu.ucsal.fiadopay.dto.PaymentResponse;
 import edu.ucsal.fiadopay.domain.Payment;
 import edu.ucsal.fiadopay.repo.PaymentRepository;
+import edu.ucsal.fiadopay.annotations.AntiFraud;
+import edu.ucsal.fiadopay.domain.FraudRules;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,34 +27,41 @@ public class PaymentService {
   private final WebhookDeliveryService webhookDeliveryService;
   private final PaymentProcessor paymentProcessor;
   private final PaymentRepository payments;
+  private final FraudRules fraudRules;
 
-    public PaymentService(MerchantService merchantService, WebhookDeliveryService webhookDeliveryService, PaymentProcessor paymentProcessor, PaymentRepository payments) {
+  public PaymentService(MerchantService merchantService, WebhookDeliveryService webhookDeliveryService, PaymentProcessor paymentProcessor, PaymentRepository payments, FraudRules fraudRules) {
         this.merchantService = merchantService;
         this.webhookDeliveryService = webhookDeliveryService;
         this.paymentProcessor = paymentProcessor;
         this.payments = payments;
+        this.fraudRules = fraudRules;
     }
 
-    @Transactional
-  public PaymentResponse createPayment(String auth, String idemKey, PaymentRequest req){
+  @Transactional
+  public PaymentResponse createPayment(String auth, String idemKey, PaymentRequest req) {
     var merchant = merchantService.merchantFromAuth(auth);
     var mid = merchant.getId();
 
+    if (isFraud(req)) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pagamento rejeitado por regras de antifraude");
+    }
+
     if (idemKey != null) {
       var existing = payments.findByIdempotencyKeyAndMerchantId(idemKey, mid);
-      if(existing.isPresent()) return toResponse(existing.get());
+      if (existing.isPresent())
+        return toResponse(existing.get());
     }
 
     BigDecimal total = calcularTotalComJuros(req);
     Double interest = (!total.equals(req.amount()) ? 1.0 : null);
 
     var payment = Payment.builder()
-        .id("pay_"+UUID.randomUUID().toString().substring(0,8))
+        .id("pay_" + UUID.randomUUID().toString().substring(0, 8))
         .merchantId(mid)
         .method(req.method().toUpperCase())
         .amount(req.amount())
         .currency(req.currency())
-        .installments(req.installments()==null?1:req.installments())
+        .installments(req.installments() == null ? 1 : req.installments())
         .monthlyInterest(interest)
         .totalWithInterest(total)
         .status(Payment.Status.PENDING)
@@ -69,32 +78,50 @@ public class PaymentService {
     return toResponse(payment);
   }
 
-  public BigDecimal calcularTotalComJuros(PaymentRequest req) {
-      PaymentCalculator calculator = new PaymentCalculator();
-
-      for (Method m : calculator.getClass().getDeclaredMethods()) {
-          if (m.isAnnotationPresent(PaymentMethod.class)) {
-              PaymentMethod annot = m.getAnnotation(PaymentMethod.class);
-              if (annot.type().equalsIgnoreCase(req.method())) {
-                  try {
-                      return (BigDecimal) m.invoke(calculator, req);
-                  } catch (IllegalAccessException | InvocationTargetException e) {
-                      throw new RuntimeException(e);
-                  }
-              }
+  public boolean isFraud(PaymentRequest req) {
+    for (Method m : fraudRules.getClass().getDeclaredMethods()) {
+      if (m.isAnnotationPresent(AntiFraud.class)) {
+        try {
+          boolean isFraudulent = (boolean) m.invoke(fraudRules, req);
+          if (isFraudulent) {
+            AntiFraud annotation = m.getAnnotation(AntiFraud.class);
+            System.out.println("Fraude detectada pela regra: " + annotation.name());
+            return true;
           }
-
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
-
-      return req.amount();
+    }
+    return false;
   }
 
-  public PaymentResponse getPayment(String id){
+  public BigDecimal calcularTotalComJuros(PaymentRequest req) {
+    PaymentCalculator calculator = new PaymentCalculator();
+
+    for (Method m : calculator.getClass().getDeclaredMethods()) {
+      if (m.isAnnotationPresent(PaymentMethod.class)) {
+        PaymentMethod annot = m.getAnnotation(PaymentMethod.class);
+        if (annot.type().equalsIgnoreCase(req.method())) {
+          try {
+            return (BigDecimal) m.invoke(calculator, req);
+          } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+          }
+        }
+      }
+
+    }
+
+    return req.amount();
+  }
+
+  public PaymentResponse getPayment(String id) {
     return toResponse(payments.findById(id)
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
   }
 
-  public Map<String,Object> refund(String auth, String paymentId){
+  public Map<String, Object> refund(String auth, String paymentId) {
     var merchant = merchantService.merchantFromAuth(auth);
     var p = payments.findById(paymentId)
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -105,22 +132,23 @@ public class PaymentService {
     p.setUpdatedAt(Instant.now());
     payments.save(p);
     webhookDeliveryService.sendWebhook(p);
-    return Map.of("id","ref_"+UUID.randomUUID(),"status","PENDING");
+    return Map.of("id", "ref_" + UUID.randomUUID(), "status", "PENDING");
   }
 
-  protected static String hmac(String payload, String secret){
+  protected static String hmac(String payload, String secret) {
     try {
       var mac = javax.crypto.Mac.getInstance("HmacSHA256");
       mac.init(new javax.crypto.spec.SecretKeySpec(secret.getBytes(), "HmacSHA256"));
       return Base64.getEncoder().encodeToString(mac.doFinal(payload.getBytes()));
-    } catch (Exception e){ return ""; }
+    } catch (Exception e) {
+      return "";
+    }
   }
 
-  private PaymentResponse toResponse(Payment p){
+  private PaymentResponse toResponse(Payment p) {
     return new PaymentResponse(
         p.getId(), p.getStatus().name(), p.getMethod(),
         p.getAmount(), p.getInstallments(), p.getMonthlyInterest(),
-        p.getTotalWithInterest()
-    );
+        p.getTotalWithInterest());
   }
 }
